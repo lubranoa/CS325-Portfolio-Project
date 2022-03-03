444b0463a942e42e1e529c35f6f56ac70b321f34
Alexander Lubrano <lubranoa@oregonstate.edu>
2022-03-02 19:19:39 -0600

    Add boilerplate function definitions
    Add boilerplate function definition that takes a board, source cell, and
    destination cell and returns a 2-tuple of the minimum number of cells
    traversed and the moves that the algorithm made to get there
    
    Add __main__ function

--- Puzzle.py
+++ Puzzle.py
@@ -5,3 +5,29 @@
 # Description:
 #
 # -----------------------------------------------------------------------------
+
+
+def solve_puzzle(board, src, dest):
+    """
+
+    :param board:
+    :param src:
+    :param dest:
+    :return:
+    """
+
+    return []
+
+
+if __name__ == '__main__':
+
+    puzzle = [['-', '-', '-', '-', '-'],
+              ['-', '-', '#', '-', '-'],
+              ['-', '-', '-', '-', '-'],
+              ['#', '-', '#', '#', '-'],
+              ['-', '#', '-', '-', '-']]
+
+    start = (1, 3)
+    end = (3, 3)
+
+    solution = solve_puzzle(puzzle, start, end)
